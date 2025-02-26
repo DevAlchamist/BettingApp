5722ae65f8bfb498d59f7f554ee32d2f56fe651e
nitinjha04 <nitinjha890@gmail.com>
2025-02-26 20:52:45 +0530

    redux

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/toaster";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
+import { Provider } from "react-redux";
+import { store } from "@/store/store";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,18 +19,20 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <div className="flex min-h-screen flex-col">
-            <div className="w-full">
-              <Navbar />
+        <Provider store={store}>
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            <div className="flex min-h-screen flex-col">
+              <div className="w-full">
+                <Navbar />
+              </div>
+              <main className="flex-1">{children}</main>
+              <div className="w-full">
+                <Footer />
+              </div>
             </div>
-            <main className="flex-1">{children}</main>
-            <div className="w-full">
-              <Footer />
-            </div>
-          </div>
-          <Toaster />
-        </ThemeProvider>
+            <Toaster />
+          </ThemeProvider>
+        </Provider>
       </body>
     </html>
   );
